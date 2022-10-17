d4890172558c3a32475480695cae2b4c4365dd0d
Mark Shepherd <markshep1@hotmail.com>
2022-10-17 20:02:45 +0100

    Add functions to remove and draw gameboard grid within gameboard module

--- game.js
+++ game.js
@@ -1,15 +1,26 @@
 
 const gameboard = (() => {
-    let tilesArray = new Array(9);
-    let arrayLength = tilesArray.length;
+    //let tilesArray = ['X', 'O', 'O', 'X', 'X', 'O', 'O', 'X', 'X']
+    let tilesArray = Array(undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined)
+    //let arrayLength = tilesArray.length;
     let container = document.querySelector('.container');
-    for(let i = 0; i < arrayLength; i++) {
-        let square = document.createElement('div')
-        square.className = 'square';
-        square.style.width = (450 / 3) + 'px';
-        square.style.height = (450 / 3) + 'px';
-        container.appendChild(square);
+    let removeSquares = () => {
+        container.innerHTML = ''
     }
-
-
+    let drawSquares = () => {
+        tilesArray.forEach(tile => {
+            let square = document.createElement('div')
+            square.className = 'square';
+            square.style.width = (450 / 3) + 'px';
+            square.style.height = (450 / 3) + 'px';
+            square.textContent = tile
+            container.appendChild(square);
+        })
+    }
+    drawSquares();
+    return {
+        tilesArray,
+        drawSquares,
+        removeSquares
+    }
 })();
